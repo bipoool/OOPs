a21e9b3a8b5fa1a5e2b5d336c5a79c0e3d05c2d0
vipul gupta <itsmevipulgupta.2011@gmail.com>
2021-07-06 19:54:36 +0530

    all fields are ready to use, not connected to database, u can customize the validations

--- CRUD.php
+++ CRUD.php
@@ -1,5 +1,8 @@
 <?php 
 
+    ini_set('display_errors', 1);
+    ini_set('display_startup_errors', 1);
+    error_reporting(E_ALL);
     //Class to create connections to databases
     class database{
 
@@ -84,7 +87,7 @@
                 $query .= " LIMIT " . $limit;
             }
             $result = $this->prep_and_run($query);
-            print_r($result);
+            return $result;
         }
 
         //function to delete data
@@ -111,7 +114,7 @@
             }
 
             $result = $this->prep_and_run($query);
-            //print_r($result);
+            return $result;
         }
 
         public function addData($table, $values){
@@ -129,7 +132,7 @@
             $query = "INSERT INTO $table ($fields) VALUES ($value)";
 
             $result = $this->prep_and_run($query);
-            //print_r($result);
+            return $result;
 
         }
 
@@ -164,12 +167,441 @@
                 $i++;    
             } 
             $result = $this->prep_and_run($query);
-            //print_r($result);
+            return $result;
 
         }
 
     }
 
+    class make{
+
+        protected function makeLabel($label, $labelAttr = array()){
+
+            $labelScript = "";
+            if($label){
+                $labelScript .= "<label "; 
+                foreach($labelAttr as $k=>$v){
+                    $labelScript .= "$k = '$v' ";
+                }
+                $labelScript .= ">$label</label>\n";
+            }
+            
+            return $labelScript;
+            
+            
+        }
+
+        protected function makeField($type, $fieldAttr = array()){
+
+            $fieldScript = "<input type = '$type' ";
+            foreach($fieldAttr as $k=>$v){
+                $fieldScript .= "$k = '$v' ";
+            }
+            $fieldScript .= ">\n";
+            return $fieldScript;
+
+        }
+
+    }
+
+    class textField extends make{
+
+        public $script;
+        protected $fieldName;
+
+        function __construct($fieldAttr = array(), $label = "", $labelAttr = array()){
+            
+            $labelScript = $this->makeLabel($label, $labelAttr);
+            $fieldScript = $this->makeField("text", $fieldAttr);
+            $this->fieldName = $fieldAttr["name"]; 
+            $this->script = array("labelScript"=>$labelScript, "fieldScript"=>$fieldScript);
+                
+        }
+        
+        public function validated(){
+
+            $data = $_POST[$this->fieldName];
+            $pattern = "/^[a-zA-Z0-9]*$/";
+            if(!preg_match($pattern, $data)){
+                $_POST[$this->fieldName] = "data is Currupted...";
+                return false;
+            }
+            return true;
+            
+        }
+
+        public function getFieldName(){
+            return $this->fieldName;
+        }
+
+    }
+
+    class passwordField extends make{
+
+        public $script;
+        protected $fieldName;
+
+        function __construct($fieldAttr = array(), $label = "", $labelAttr = array()){
+            
+            $labelScript = $this->makeLabel($label, $labelAttr);
+            $fieldScript = $this->makeField("password", $fieldAttr);
+            $this->fieldName = $fieldAttr["name"]; 
+            $this->script = array("labelScript"=>$labelScript, "fieldScript"=>$fieldScript);
+                
+        }
+        
+        public function validated(){
+
+            $data = $_POST[$this->fieldName];
+            $pattern = "/^[a-zA-Z0-9]*$/";
+            if(!preg_match($pattern, $data)){
+                $_POST[$this->fieldName] = "data is Currupted...";
+                return false;
+            }
+            return true;
+            
+        }
+
+        public function getFieldName(){
+            return $this->fieldName;
+        }
+  
+
+    }
+
+    class emailField extends make{
+
+        public $script;
+        protected $fieldName;
+
+        function __construct($fieldAttr = array(), $label = "", $labelAttr = array()){
+            
+            $labelScript = $this->makeLabel($label, $labelAttr);
+            $fieldScript = $this->makeField("email", $fieldAttr);
+            $this->fieldName = $fieldAttr["name"]; 
+            $this->script = array("labelScript"=>$labelScript, "fieldScript"=>$fieldScript);
+                
+        }
+        
+        public function validated(){
+
+            $data = $_POST[$this->fieldName];
+            $pattern = "/^[a-zA-Z0-9]*$/";
+            if(!preg_match($pattern, $data)){
+                $_POST[$this->fieldName] = "data is Currupted...";
+                return false;
+            }
+            return true;
+            
+        }
+
+        public function getFieldName(){
+            return $this->fieldName;
+        }
+        
+
+    }
+
+    class dateField extends make{
+
+        public $script;
+        protected $fieldName;
+
+        function __construct($fieldAttr = array(), $label = "", $labelAttr = array()){
+            
+            $labelScript = $this->makeLabel($label, $labelAttr);
+            $fieldScript = $this->makeField("date", $fieldAttr);
+            $this->fieldName = $fieldAttr["name"]; 
+            $this->script = array("labelScript"=>$labelScript, "fieldScript"=>$fieldScript);
+                
+        }
+
+        public function getFieldName(){
+            return $this->fieldName;
+        }
+      
+
+    }
+
+    class timeField extends make{
+
+        public $script;
+        protected $fieldName;
+
+        function __construct($fieldAttr = array(), $label = "", $labelAttr = array()){
+            
+            $labelScript = $this->makeLabel($label, $labelAttr);
+            $fieldScript = $this->makeField("time", $fieldAttr);
+            $this->fieldName = $fieldAttr["name"]; 
+            $this->script = array("labelScript"=>$labelScript, "fieldScript"=>$fieldScript);
+                
+        }
+        
+        public function getFieldName(){
+            return $this->fieldName;
+        }
+       
+
+    }
+
+    class checkboxField extends make{
+
+        public $script;
+        protected $fieldName;
+
+        function __construct($fieldAttr = array(), $label = "", $labelAttr = array()){
+            
+            $labelScript = $this->makeLabel($label, $labelAttr);
+            $fieldScript = $this->makeField("checkbox", $fieldAttr);
+            $this->fieldName = $fieldAttr["name"]; 
+            $this->script = array("labelScript"=>$labelScript, "fieldScript"=>$fieldScript);
+                
+        }
+
+        public function getFieldName(){
+            return $this->fieldName;
+        }
+       
+
+    }
+
+    class radioField extends make{
+
+        public $script;
+        protected $fieldName;
+
+        function __construct($fieldAttr = array(), $label = "", $labelAttr = array()){
+            
+            $labelScript = $this->makeLabel($label, $labelAttr);
+            $fieldScript = $this->makeField("text", $fieldAttr);
+            $this->fieldName = $fieldAttr["name"]; 
+            $this->script = array("labelScript"=>$labelScript, "fieldScript"=>$fieldScript);
+                
+        }
+
+        public function getFieldName(){
+            return $this->fieldName;
+        }
+     
+
+    }
+
+    class textArea{
+
+        public $script;
+        protected $fieldName;
+        function __construct($fieldAttr = array(), $label = "", $labelAttr = array()){
+
+            $labelScript = "";
+            if($label){
+                $labelScript .= "<label "; 
+                foreach($labelAttr as $k=>$v){
+                    $labelScript .= "$k = '$v' ";
+                }
+                $labelScript .= ">$label</label>\n";
+            }
+
+            $fieldScript = "<textarea ";
+            foreach($fieldAttr as $k=>$v){
+                $fieldScript .= "$k = '$v' ";
+            }
+            $fieldScript .= "></textarea>\n";
+            $this->script = array("labelScript"=>$labelScript, "fieldScript"=>$fieldScript);
+        }
+
+        public function validated(){
+
+            $data = $_POST[$this->fieldName];
+            $pattern = "/^[a-zA-Z0-9]*$/";
+            if(!preg_match($pattern, $data)){
+                $_POST[$this->fieldName] = "data is Currupted...";
+                return false;
+            }
+            return true;
+            
+        }
+
+        public function getFieldName(){
+            return $this->fieldName;
+        }
+
+    }
+    
+    class submitButtonField extends make{
+
+        public $script;
+        function __construct($fieldAttr = array(), $label = "", $labelAttr = array()){
+            
+            $labelScript = $this->makeLabel($label, $labelAttr);
+            $fieldScript = $this->makeField("submit", $fieldAttr);
+            $this->script = array("labelScript"=>$labelScript, "fieldScript"=>$fieldScript);
+                
+        }        
+
+    }
+
+    class form{
+
+        //global variables
+        protected $script;
+        protected $fields;
+        protected $labels;
+        protected $validatedData;
+        protected $fieldsArray;
+        protected $submitButtonName;
+        protected $fieldsError;
+
+        public function __construct(){
+            $this->script = "";
+            $this->fields = array();
+            $this->labels = array();
+            $this->fieldsArray = array();
+            $this->submitButtonName = array();
+            $this->fieldsError = array();
+        }
+
+        public function updatingGlobals($fieldAttr, $labelScript, $fieldScript){
+            if($fieldAttr["name"]){
+                $this->labels[$fieldAttr["name"]] = $labelScript;
+                $this->fields[$fieldAttr["name"]] = $fieldScript;
+            }
+            $this->fieldsError[$fieldAttr["name"]] = "";
+            $this->script .= $labelScript . $fieldScript;
+        }
+
+
+        public function addTextField($fieldAttr = array(), $label = "", $labelAttr = array()){
+            
+            $field = new textField($fieldAttr, $label, $labelAttr);
+            $this->updatingGlobals($fieldAttr ,$field->script["labelScript"], $field->script["fieldScript"]);
+            $this->fieldsArray[$fieldAttr["name"]] = $field;
+                
+        }
+
+        public function addPasswordField($fieldAttr = array(), $label = "", $labelAttr = array()){
+            
+            $field = new passwordField($fieldAttr, $label, $labelAttr);
+            $this->updatingGlobals($fieldAttr ,$field->script["labelScript"], $field->script["fieldScript"]);
+            $this->fieldsArray[$fieldAttr["name"]] = $field;
+                
+        }
+
+        public function addEmailField($fieldAttr = array(), $label = "", $labelAttr = array()){
+            
+            $field = new emailField($fieldAttr, $label, $labelAttr);
+            $this->updatingGlobals($fieldAttr ,$field->script["labelScript"], $field->script["fieldScript"]);
+            $this->fieldsArray[$fieldAttr["name"]] = $field;
+                
+        }
+
+        public function addDateField($fieldAttr = array(), $label = "", $labelAttr = array()){
+            
+            $field = new dateField($fieldAttr, $label, $labelAttr);
+            $this->updatingGlobals($fieldAttr ,$field->script["labelScript"], $field->script["fieldScript"]);
+            $this->fieldsArray[$fieldAttr["name"]] = $field;
+
+        }
+
+        public function addTimeField($fieldAttr = array(), $label = "", $labelAttr = array()){
+            
+            $field = new timeField($fieldAttr, $label, $labelAttr);
+            $this->updatingGlobals($fieldAttr ,$field->script["labelScript"], $field->script["fieldScript"]);   
+            $this->fieldsArray[$fieldAttr["name"]] = $field;           
+        }
+
+        public function addCheckBoxField($fieldAttr = array(), $label = "", $labelAttr = array()){
+
+            $field = new checkboxField($fieldAttr, $label, $labelAttr);
+            $this->updatingGlobals($fieldAttr ,$field->script["labelScript"], $field->script["fieldScript"]);
+            $this->fieldsArray[$fieldAttr["name"]] = $field;
+        }
+
+        public function addRadioField($fieldAttr = array(), $label = "", $labelAttr = array()){
+
+            $field = new radioField($fieldAttr, $label, $labelAttr);
+            $fieldScript = $field->script["fieldScript"];
+            $labelScript = $field->script["labelScript"];
+
+            //names of all radioButton are same so we are saving it as id
+            if($fieldAttr["id"]){
+                $this->fields[$fieldAttr["id"]] = $fieldScript;
+                $this->labels[$fieldAttr["id"]] = $labelScript;
+            }
+            $this->script .= $labelScript . $fieldScript;
+            $this->fieldsArray[$fieldAttr["id"]] = $field;
+        }
+
+        public function addTextAreaField($fieldAttr = array(), $label = "", $labelAttr = array()){
+
+            $field = new textArea($fieldAttr, $label, $labelAttr);
+            $this->updatingGlobals($fieldAttr ,$field->script["labelScript"], $field->script["fieldScript"]);
+            $this->fieldsArray[$fieldAttr["name"]] = $field;
+
+        }
+
+        public function addSubmitButton($fieldAttr = array(), $label = "", $labelAttr = array()){
+
+            $field = new submitButtonField($fieldAttr, $label, $labelAttr);
+            $this->updatingGlobals($fieldAttr ,$field->script["labelScript"], $field->script["fieldScript"]);
+            $this->submitButtonName = $fieldAttr["name"];
+
+        }
+
+        public function validate(){
+
+            $result = array();
+            if(isset($_POST[$this->submitButtonName])){
+                foreach($this->fieldsArray as $field){
+                    $fieldName = $field->getFieldName();
+                    if($_POST[$fieldName]==""){
+                        $fieldName = $field->getFieldName();
+                        $this->fieldsError[$fieldName] = "Field Is Empty";
+                    }
+                    if($field->validated()){
+                        $fieldName = $field->getFieldName();
+                        $result[$fieldName] = $_POST[$fieldName]; 
+                    }
+                    else{
+                        $fieldName = $field->getFieldName();
+                        $result[$fieldName] = "data is currupted";
+                        $this->fieldsError[$fieldName] = "Data is not valid";
+                    }
+                }
+                
+            }
+            return $result;
+
+        }
+
+        public function getField($name_or_id_if_radioButton){
+
+            return $this->fields[$name_or_id_if_radioButton];
+
+        }
+        public function getLabel($name_or_id_if_radioButton){
+
+            return $this->labels[$name_or_id_if_radioButton];
+
+        }
+
+        public function render_as_p(){
+
+            foreach($this->fields as $k=>$v){
+
+                if($this->fieldsError[$k]){
+                    echo "<div class='alert alert-danger'>". $this->fieldsError[$k] . "</div>"; 
+                }
+                echo "<p>" . $this->labels[$k] . $v . "</p>";
+            }
+            
+        }
+
+        public function render(){
+            echo $this->script;
+        }
+
+
+    }
 
 
 
--- test.php
+++ test.php
@@ -4,9 +4,49 @@
 
     $query = new query();
 
-    $query->getData("Users", "*");
+    //$query->getData("Users", "*");
     //$query->deleteData("Users", array("Name" => "VIPUL"));
     //$query->addData("Users", array("Name"=>"VIPUL", "Email"=>"vipul@gmail.com", "Password" => "vipul","Age"=>20));
     //$query->updateData("Users", array("Name"=>"Raman Bansal"), array("Name"=>"Raman Bansal", "Age" => 9));
 
+    $form = new form();
+    $form->addTextField(array("name"=>"userName", "placeholder" => "username", "value"=>"vip3022", "class"=>"form-control"), "UserName",array("class"=>"label-lg"));
+    // $form->addPasswordField(array("name"=>"password", "placeholder" => "Password", "class"=>"form-control"), "Password", array("class"=>"label-lg"));
+    $form->addSubmitButton(array("name"=>"submit", "class"=>"btn btn-success"), "", array("class"=>"label-lg"));
+    print_r($form->validate());
+
 ?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css" integrity="sha384-BVYiiSIFeK1dGmJRAkycuHAHRg32OmUcww7on3RYdg4Va+PmSTsz/K68vbdEjh4u" crossorigin="anonymous">
+
+</head>
+<body>
+    
+    <h1>Form Class Test</h1>
+    <form action="" method="post" class = "container col-sm-6">
+    
+        <?php
+           $form->render_as_p();
+        ?>
+        
+    </form>
+
+
+    <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js" integrity="sha384-Tc5IQib027qvyjSMfHjOMaLkfuWVxZxUPnCJA7l2mCWNIpG9mGCD8wGNIcPD7Txa" crossorigin="anonymous"></script>
+
+
+</body>
+</html>
+
+
+
+
+
